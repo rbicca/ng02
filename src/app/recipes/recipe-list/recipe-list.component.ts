dfb16d5120df8f3fda85ee153deba72a3b41281e
Ronaldo Bicca <ronaldo.bicca@gmail.com>
2023-11-11 17:42:14 -0300

    Cap 062

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe('Bife a Parmegiana', 'Receita bem empanada...', 'https://img.saborosos.com.br/imagens/bife-a-parmegiana.jpg'),
+    new Recipe('Estrogonofe', 'Carne picada com...', 'https://www.receitasnestle.com.br/sites/default/files/srh_recipes/861d71239103ef70f76554abf688bfc8.jpg'),
+  ];
 }
